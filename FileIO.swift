4b11dbaa55a36d177a0c7ed29ac9301643cf37c9
Noah Smith <noah.smith@stu.ocsb.ca>
2025-03-07 16:08:31 +0000

    Style errors

--- FileIO.swift
+++ FileIO.swift
@@ -95,7 +95,7 @@ do {
 
                 // Increment validInt
                 validInt += 1
-    
+
             } else {
                 // Display error message in the output file
                 outputStr += "\(numStr) is not a valid integer. \n"
